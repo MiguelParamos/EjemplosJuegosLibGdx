18b4366bceaf4e5ab7d157202cba883e622c5bff
Miguel Páramos <miguel.paramos@cenecmalaga.es>
2020-01-24 11:02:49 +0100

    Metida la base del juego Prendele fuego
    Metida la base del juego Prendele fuego

---
+++ PrendeleFuego/desktop/src/ahora/prendele/fuego/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package ahora.prendele.fuego.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import ahora.prendele.fuego.Juego;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Juego(), config);
+	}
+}
