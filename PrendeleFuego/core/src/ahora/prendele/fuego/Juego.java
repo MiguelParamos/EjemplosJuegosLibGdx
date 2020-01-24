9e3c958e7c996717d9b989edb76bafa09c586c52
Miguel Páramos <miguel.paramos@cenecmalaga.es>
2020-01-24 11:17:38 +0100

    Preparados los elementos iniciales del juego
    Preparados los elementos iniciales del juego

--- PrendeleFuego/core/src/ahora/prendele/fuego/Juego.java
+++ PrendeleFuego/core/src/ahora/prendele/fuego/Juego.java
@@ -3,13 +3,18 @@ package ahora.prendele.fuego;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 
 public class Juego extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
+	private OrthogonalTiledMapRenderer renderer; //Clase auxiliar para renderizar un mapa.
+	private OrthographicCamera camera; //Cámara a través de la que veremos el mundo.
+	private static int WIDTH; //Aquí almacenaremos la anchura en tiles
+	private static int HEIGHT; //Aquí almacenaremos la altura en tiles
+	public static final float unitScale = 1/32f; //Nos servirá para establecer que la pantalla se divide en tiles de 32 pixeles;
+
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
