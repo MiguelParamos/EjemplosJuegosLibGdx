be8880cf538ee62a225e0c745849e92f51ad0c33
Miguel Páramos <miguel.paramos@cenecmalaga.es>
2020-01-27 12:11:43 +0100

    Metido el proyecto por defecto de mundo contínuo
    Metido el proyecto por defecto de mundo contínuo

---
+++ Mundo Continuo/desktop/src/es/cenec/mundocontinuo/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package es.cenec.mundocontinuo.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import es.cenec.mundocontinuo.Juego;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Juego(), config);
+	}
+}
