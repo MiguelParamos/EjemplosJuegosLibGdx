c59a17faf81ec252765269a7dedd5ed25f77e699
Miguel Páramos <miguel.paramos@cenecmalaga.es>
2020-02-07 11:13:06 +0100

    Metida base del juego de físicas
    Metida base del juego de físicas

---
+++ Juego Fisicas/desktop/src/es/cenecmalaga/fisicas/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package es.cenecmalaga.fisicas.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import es.cenecmalaga.fisicas.Juego;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Juego(), config);
+	}
+}
