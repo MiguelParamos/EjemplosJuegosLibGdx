87291e6eca3988ebc28c089b43862ab8d87c6590
Miguel Páramos <miguel.paramos@cenecmalaga.es>
2020-02-10 12:59:49 +0100

    Maltratando Pollos
    Nigún pollo real fué herido en el desarrollo de este programa, pero muchos pollos virutales siguen cayendo todavía sin nada contra lo que estamparse.

--- Juego Fisicas/core/src/Personajes/Pollo.java
+++ Juego Fisicas/core/src/Personajes/Pollo.java
@@ -4,30 +4,57 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
+import com.badlogic.gdx.physics.box2d.World;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 
 import java.util.Random;
 
-public class Pollo extends Actor {
+public class Pollo  {
     protected Sprite sprite;
+    private World mundo;
+    private BodyDef propiedadesCuerpo;
+    private Body cuerpo;
+    private FixtureDef propiedadesFisicasCuerpo;
 
-    public Pollo(){
+
+    public Pollo(World m){
+        mundo=m;
         sprite=new Sprite(new Texture("personajes/pollo.png"));
-        Random r=new Random();
-        int anchuraSprite=Gdx.graphics.getWidth()/10;
-        int alturaSprite=Gdx.graphics.getHeight()/7;
-        sprite.setBounds(r.nextInt((Gdx.graphics.getWidth()-anchuraSprite)),
-                r.nextInt((Gdx.graphics.getHeight()-alturaSprite)),
-                anchuraSprite,alturaSprite);
-        this.setPosition(sprite.getX(),sprite.getY());
-        this.setSize(Gdx.graphics.getWidth()/10,Gdx.graphics.getHeight()/7);
-        this.setOrigin(this.sprite.getWidth()/2,this.sprite.getHeight()/2);
-        sprite.setOrigin(this.getOriginX(),this.getOriginY());
+        int anchuraSprite=1; //Anchura y altura se expresan ahora en metros
+        int alturaSprite=1;//Anchura y altura se expresan ahora en metros
+        sprite.setBounds(3,8,
+                anchuraSprite,alturaSprite); //La posición inicial también debe estar en metros
+
+        this.propiedadesCuerpo= new BodyDef(); //Establecemos las propiedades del cuerpo
+        propiedadesCuerpo.type = BodyDef.BodyType.DynamicBody;
+        propiedadesCuerpo.position.set(sprite.getX(), sprite.getY());
+
+        cuerpo = mundo.createBody(propiedadesCuerpo);
+
+        propiedadesFisicasCuerpo = new FixtureDef();
+        propiedadesFisicasCuerpo.shape = new PolygonShape();
+        ((PolygonShape)propiedadesFisicasCuerpo.shape).setAsBox(anchuraSprite/2f, alturaSprite/2f);
+        propiedadesFisicasCuerpo.density = 1f;
+        cuerpo.createFixture(propiedadesFisicasCuerpo);
+
+        sprite.setOrigin(this.sprite.getWidth()/2,
+                this.sprite.getHeight()/2);
+
+
     }
 
-    @Override
-    public void draw(Batch batch, float parentAlpha) { sprite.draw(batch);
-        //No necesitamos inicializar ni finalizar el batch: Se hace automáticamente.
+
+    public void draw(Batch batch, float parentAlpha) {
+        //Esta cuenta hace falta por lo de la media altura. Ese absurdo cálculo...
+        sprite.setPosition(cuerpo.getPosition().x-sprite.getWidth()/2,cuerpo.getPosition().y-sprite.getHeight()/2);
+        //Sprite quiere la rotación en grados, el cuerpo la da en radianes. Esta constante convierte de uno a otro.
+        sprite.setRotation(MathUtils.radiansToDegrees*cuerpo.getAngle());
+        sprite.draw(batch);
     }
 
 }
--- Juego Fisicas/core/src/es/cenecmalaga/fisicas/Juego.java
+++ Juego Fisicas/core/src/es/cenecmalaga/fisicas/Juego.java
@@ -4,32 +4,50 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
+import com.badlogic.gdx.physics.box2d.World;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import com.sun.org.apache.xpath.internal.operations.Or;
 
 import Personajes.Pollo;
 
 public class Juego extends Game {
-	Stage stage;
+	private World world;
+	private Pollo pollo;
+	private Box2DDebugRenderer debugRenderer;
+	private SpriteBatch batch;
+	private OrthographicCamera camara;
 	
 	@Override
 	public void create () {
-		stage=new Stage(new ScreenViewport());
-		stage.addActor(new Pollo());
+		batch=new SpriteBatch();
+		world=new World(new Vector2(0,-9.8f),true);
+		pollo=new Pollo(world);
+		camara=new OrthographicCamera(10,10);
+		this.debugRenderer=new Box2DDebugRenderer();
 	}
 
 	@Override
 	public void render () {
 		Gdx.gl.glClearColor(1, 1, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		stage.draw();
+		world.step(Gdx.graphics.getDeltaTime(), 6, 2);
+		batch.setProjectionMatrix(camara.combined);
+		batch.begin();
+		pollo.draw(batch,0);
+		batch.end();
 
+		camara.update();
+		debugRenderer.render(world, camara.combined);
 	}
 	
 	@Override
 	public void dispose () {
-		stage.dispose();
+		world.dispose();
 	}
 }
